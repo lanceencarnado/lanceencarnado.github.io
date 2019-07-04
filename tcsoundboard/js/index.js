18b11955bcd42373b95c54d758cb1e043e560c36
lscorrcho <l.scorrcho@gmail.com>
2019-07-03 20:26:03 -0400

    Revert "differentiating between quotes and drops visually"
    This reverts commit 1442db7d9b39cc7beff7c71b0a39bd855c45835c.

--- tcsoundboard/js/index.js
+++ tcsoundboard/js/index.js
@@ -49,26 +49,19 @@ class Board extends React.Component {
 ;
 
 class Sound extends React.Component {
-	render() {
-		let speakerStyle = 'fa fa-volume-off fa-3x';
-		if (this.props.sound.isPlaying && this.props.sound.soundName === this.props.audio.soundName && this.props.audio.currentlyPlaying) {
-	  		speakerStyle += 'fa fa-volume-up fa-3x';
-		}
-		if (this.props.sound.type == "drop") {
-			return (
-				React.createElement("div", { className: "sound-card-drop",
-					onClick: () => this.props.playSound(this.props.sound.id) },
-		  		React.createElement("div", { className: "sound-card-text" }, this.props.sound.soundName))
-		  	);
-		} else {
-			return (
-				React.createElement("div", { className: "sound-card",
-					onClick: () => this.props.playSound(this.props.sound.id) },
-				React.createElement("div", { className: "sound-card-text" }, this.props.sound.soundName))
-		 	);
-	 	}
+  render() {
+	let speakerStyle = 'fa fa-volume-off fa-3x';
+	if (this.props.sound.isPlaying && this.props.sound.soundName === this.props.audio.soundName && this.props.audio.currentlyPlaying) {
+	  speakerStyle += 'fa fa-volume-up fa-3x';
 	}
-}
+	return (
+	  React.createElement("div", { className: "sound-card",
+		onClick: () => this.props.playSound(this.props.sound.id) },
+	  React.createElement("div", { className: "sound-card-text" }, this.props.sound.soundName)));
+
+
+
+  }}
 
 
 class App extends React.Component {
@@ -76,25 +69,25 @@ class App extends React.Component {
 	super(props);
 	this.state = {
 	  sounds: [
-	  { id: 1, soundName: 'TIME CRISIS INTRO', soundURL: './sounds/tcintro.mp3', isPlaying: false, type: "drop" },
-	  { id: 2, soundName: 'SEINFELD NUMBER CRUNCH', soundURL: './sounds/seinfeldnumbercrunch.mp3', isPlaying: false, type: "quote" },
-	  { id: 3, soundName: 'DIET COKE: BECAUSE I CAN', soundURL: './sounds/dietcoke.mp3', isPlaying: false, type: "drop" },
-	  { id: 4, soundName: 'TIME CRISIS MAILBAG', soundURL: './sounds/tcmailbag.mp3', isPlaying: false, type: "drop" },
-	  { id: 5, soundName: 'BORGESIAN', soundURL: './sounds/borgesian.mp3', isPlaying: false, type: "quote" },
-	  { id: 6, soundName: 'ROCK\'S PLAYED OUT, DOG', soundURL: './sounds/rocksplayedout.mp3', isPlaying: false, type: "quote" },
-	  { id: 7, soundName: '8 MINUTE CAPE COD', soundURL: './sounds/8minutecapecod.mp3', isPlaying: false, type: "drop" }, 
-	  { id: 8, soundName: 'THIS SHOW RULES', soundURL: './sounds/thisshowrules.mp3', isPlaying: false, type: "quote" }, 
-	  { id: 9, soundName: 'BUT IN MY NEIGHBOR- HOOD...', soundURL: './sounds/butinmyneighborhood.mp3', isPlaying: false, type: "quote" },
-	  { id: 10, soundName: 'TIME CRISIS HOTLINE', soundURL: './sounds/tchotline.mp3', isPlaying: false, type: "drop" },
-	  { id: 11, soundName: 'SWEET CHILI HEAT® WORLD PREMIERE', soundURL: './sounds/tcsweetchilipremiere.mp3', isPlaying: false, type: "drop" },
-	  { id: 12, soundName: 'BUFF TRAFFIC SITCH', soundURL: './sounds/bufftrafficsitch.mp3', isPlaying: false, type: "quote" },
-	  { id: 13, soundName: 'BRUTAL', soundURL: './sounds/brutal.mp3', isPlaying: false, type: "quote" },
-	  { id: 14, soundName: 'JAKE\'S TAKES', soundURL: './sounds/jakestakes.mp3', isPlaying: false, type: "drop" }, 
-	  { id: 15, soundName: 'TASTEFUL PALETTE OF THE 1970\'S', soundURL: './sounds/tastefulpalette.mp3', isPlaying: false, type: "drop" }, 
-	  { id: 16, soundName: 'BANKIN\' THE EPS', soundURL: './sounds/bankintheeps.mp3', isPlaying: false, type: "quote" }, 
-	  { id: 17, soundName: 'SMALL CHARCOAL GRILL', soundURL: './sounds/smallcharcoalgrill.mp3', isPlaying: false, type: "quote" }, 
-	  { id: 18, soundName: 'CORPORATE FOOD HISTORY', soundURL: './sounds/corporatefoodhistory.mp3', isPlaying: false, type: "drop" }, 
-	  { id: 19, soundName: 'AWFULLY HOT COFFEE POT', soundURL: './sounds/awfullyhotcoffeepot.mp3', isPlaying: false, type: "quote" }, 
+	  { id: 1, soundName: 'TIME CRISIS INTRO', soundURL: './sounds/tcintro.mp3', isPlaying: false },
+	  { id: 2, soundName: 'SEINFELD NUMBER CRUNCH', soundURL: './sounds/seinfeldnumbercrunch.mp3', isPlaying: false },
+	  { id: 3, soundName: 'DIET COKE: BECAUSE I CAN', soundURL: './sounds/dietcoke.mp3', isPlaying: false },
+	  { id: 4, soundName: 'TIME CRISIS MAILBAG', soundURL: './sounds/tcmailbag.mp3', isPlaying: false },
+	  { id: 5, soundName: 'BORGESIAN', soundURL: './sounds/borgesian.mp3', isPlaying: false },
+	  { id: 6, soundName: 'ROCK\'S PLAYED OUT, DOG', soundURL: './sounds/rocksplayedout.mp3', isPlaying: false },
+	  { id: 7, soundName: '8 MINUTE CAPE COD', soundURL: './sounds/8minutecapecod.mp3', isPlaying: false }, 
+	  { id: 8, soundName: 'THIS SHOW RULES', soundURL: './sounds/thisshowrules.mp3', isPlaying: false }, 
+	  { id: 9, soundName: 'BUT IN MY NEIGHBOR- HOOD...', soundURL: './sounds/butinmyneighborhood.mp3', isPlaying: false },
+	  { id: 10, soundName: 'TIME CRISIS HOTLINE', soundURL: './sounds/tchotline.mp3', isPlaying: false },
+	  { id: 11, soundName: 'SWEET CHILI HEAT® WORLD PREMIERE', soundURL: './sounds/tcsweetchilipremiere.mp3', isPlaying: false },
+	  { id: 12, soundName: 'BUFF TRAFFIC SITCH', soundURL: './sounds/bufftrafficsitch.mp3', isPlaying: false },
+	  { id: 13, soundName: 'BRUTAL', soundURL: './sounds/brutal.mp3', isPlaying: false },
+	  { id: 14, soundName: 'JAKE\'S TAKES', soundURL: './sounds/jakestakes.mp3', isPlaying: false }, 
+	  { id: 15, soundName: 'TASTEFUL PALETTE OF THE 1970\'S', soundURL: './sounds/tastefulpalette.mp3', isPlaying: false }, 
+	  { id: 16, soundName: 'BANKIN\' THE EPS', soundURL: './sounds/bankintheeps.mp3', isPlaying: false }, 
+	  { id: 17, soundName: 'SMALL CHARCOAL GRILL', soundURL: './sounds/smallcharcoalgrill.mp3', isPlaying: false }, 
+	  { id: 18, soundName: 'CORPORATE FOOD HISTORY', soundURL: './sounds/corporatefoodhistory.mp3', isPlaying: false }, 
+	  { id: 19, soundName: 'AWFULLY HOT COFFEE POT', soundURL: './sounds/awfullyhotcoffeepot.mp3', isPlaying: false }, 
 	  ]};
 
 
