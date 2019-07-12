d73d6965d7106d6900f396688703ca530a565dfd
lscorrcho <l.scorrcho@gmail.com>
2019-07-11 23:38:13 -0400

    border width is viewport width based now, sounds ordered by drops first then quotes

--- tcsoundboard/js/index.js
+++ tcsoundboard/js/index.js
@@ -65,36 +65,36 @@ class Sound extends React.Component {
 
 
 class App extends React.Component {
-  constructor(props) {
-	super(props);
-	this.state = {
-	  sounds: [
-	  { id: 1, soundName: 'TIME CRISIS INTRO (DROP)', soundURL: './sounds/tcintro.mp3', isPlaying: false },
-	  { id: 2, soundName: 'SEINFELD NUMBER CRUNCH', soundURL: './sounds/seinfeldnumbercrunch.mp3', isPlaying: false },
-	  { id: 3, soundName: 'DIET COKE: BECAUSE I CAN', soundURL: './sounds/dietcoke.mp3', isPlaying: false },
-	  { id: 4, soundName: 'TIME CRISIS MAILBAG (DROP)', soundURL: './sounds/tcmailbag.mp3', isPlaying: false },
-	  { id: 5, soundName: 'BORGESIAN', soundURL: './sounds/borgesian.mp3', isPlaying: false },
-	  { id: 6, soundName: 'ROCK\'S PLAYED OUT, DOG', soundURL: './sounds/rocksplayedout.mp3', isPlaying: false },
-	  { id: 7, soundName: '8 MINUTE CAPE COD (DROP)', soundURL: './sounds/8minutecapecod.mp3', isPlaying: false }, 
-	  { id: 8, soundName: 'THIS SHOW RULES', soundURL: './sounds/thisshowrules.mp3', isPlaying: false }, 
-	  { id: 9, soundName: 'BUT IN MY NEIGHBOR- HOOD...', soundURL: './sounds/butinmyneighborhood.mp3', isPlaying: false },
-	  { id: 10, soundName: 'TIME CRISIS HOTLINE (DROP)', soundURL: './sounds/tchotline.mp3', isPlaying: false },
-	  { id: 11, soundName: 'SWEET CHILI HEAT® WORLD PREMIERE (DROP)', soundURL: './sounds/tcsweetchilipremiere.mp3', isPlaying: false },
-	  { id: 12, soundName: 'BUFF TRAFFIC SITCH', soundURL: './sounds/bufftrafficsitch.mp3', isPlaying: false },
-	  { id: 13, soundName: 'BRUTAL', soundURL: './sounds/brutal.mp3', isPlaying: false },
-	  { id: 14, soundName: 'JAKE\'S TAKES (DROP)', soundURL: './sounds/jakestakes.mp3', isPlaying: false }, 
-	  { id: 15, soundName: 'TASTEFUL PALETTE OF THE 1970\'S (DROP)', soundURL: './sounds/tastefulpalette.mp3', isPlaying: false }, 
-	  { id: 16, soundName: 'BANKIN\' THE EPS', soundURL: './sounds/bankintheeps.mp3', isPlaying: false }, 
-	  { id: 17, soundName: 'SMALL CHARCOAL GRILL', soundURL: './sounds/smallcharcoalgrill.mp3', isPlaying: false }, 
-	  { id: 18, soundName: 'CORPORATE FOOD HISTORY (DROP)', soundURL: './sounds/corporatefoodhistory.mp3', isPlaying: false }, 
-	  { id: 19, soundName: 'AWFULLY HOT COFFEE POT', soundURL: './sounds/awfullyhotcoffeepot.mp3', isPlaying: false }, 
-	  { id: 20, soundName: 'ROUGH STUFF FOLKS 1', soundURL: './sounds/roughstufffolks1.mp3', isPlaying: false }, 
-	  { id: 21, soundName: 'ROUGH STUFF FOLKS 2', soundURL: './sounds/roughstufffolks2.mp3', isPlaying: false },
-	  { id: 22, soundName: 'PRETTY TOUGH TONE', soundURL: './sounds/prettytoughtone.mp3', isPlaying: false },
-	  { id: 23, soundName: 'OHHH!', soundURL: './sounds/boomoh.mp3', isPlaying: false },
-	  { id: 24, soundName: 'MASK OFF', soundURL: './sounds/maskoff.mp3', isPlaying: false },
-	  { id: 25, soundName: 'I\'M A VIBE GUY', soundURL: './sounds/imavibeguy.mp3', isPlaying: false },
-	  ]};
+	constructor(props) {
+		super(props);
+		this.state = {
+	  	sounds: [
+	  	 	{ id: 1, soundName: 'TIME CRISIS INTRO (DROP)', soundURL: './sounds/tcintro.mp3', isPlaying: false },
+			{ id: 4, soundName: 'TIME CRISIS MAILBAG (DROP)', soundURL: './sounds/tcmailbag.mp3', isPlaying: false },
+			{ id: 7, soundName: '8 MINUTE CAPE COD (DROP)', soundURL: './sounds/8minutecapecod.mp3', isPlaying: false },
+			{ id: 10, soundName: 'TIME CRISIS HOTLINE (DROP)', soundURL: './sounds/tchotline.mp3', isPlaying: false }, 
+			{ id: 11, soundName: 'SWEET CHILI HEAT® WORLD PREMIERE (DROP)', soundURL: './sounds/tcsweetchilipremiere.mp3', isPlaying: false },
+			{ id: 14, soundName: 'JAKE\'S TAKES (DROP)', soundURL: './sounds/jakestakes.mp3', isPlaying: false }, 
+			{ id: 15, soundName: 'TASTEFUL PALETTE OF THE 1970\'S (DROP)', soundURL: './sounds/tastefulpalette.mp3', isPlaying: false }, 
+			{ id: 18, soundName: 'CORPORATE FOOD HISTORY (DROP)', soundURL: './sounds/corporatefoodhistory.mp3', isPlaying: false }, 
+			{ id: 2, soundName: 'SEINFELD NUMBER CRUNCH', soundURL: './sounds/seinfeldnumbercrunch.mp3', isPlaying: false },
+			{ id: 6, soundName: 'ROCK\'S PLAYED OUT, DOG', soundURL: './sounds/rocksplayedout.mp3', isPlaying: false },
+			{ id: 8, soundName: 'THIS SHOW RULES', soundURL: './sounds/thisshowrules.mp3', isPlaying: false }, 
+			{ id: 9, soundName: 'BUT IN MY NEIGHBOR- HOOD...', soundURL: './sounds/butinmyneighborhood.mp3', isPlaying: false },
+			{ id: 12, soundName: 'BUFF TRAFFIC SITCH', soundURL: './sounds/bufftrafficsitch.mp3', isPlaying: false },
+			{ id: 13, soundName: 'BRUTAL', soundURL: './sounds/brutal.mp3', isPlaying: false },
+			{ id: 16, soundName: 'BANKIN\' THE EPS', soundURL: './sounds/bankintheeps.mp3', isPlaying: false }, 
+			{ id: 5, soundName: 'BORGESIAN', soundURL: './sounds/borgesian.mp3', isPlaying: false },
+			{ id: 24, soundName: 'MASK OFF', soundURL: './sounds/maskoff.mp3', isPlaying: false },
+			{ id: 3, soundName: 'DIET COKE: BECAUSE I CAN', soundURL: './sounds/dietcoke.mp3', isPlaying: false },
+			{ id: 22, soundName: 'PRETTY TOUGH TONE', soundURL: './sounds/prettytoughtone.mp3', isPlaying: false },
+			{ id: 17, soundName: 'SMALL CHARCOAL GRILL', soundURL: './sounds/smallcharcoalgrill.mp3', isPlaying: false }, 
+			{ id: 19, soundName: 'AWFULLY HOT COFFEE POT', soundURL: './sounds/awfullyhotcoffeepot.mp3', isPlaying: false }, 
+			{ id: 20, soundName: 'ROUGH STUFF FOLKS 1', soundURL: './sounds/roughstufffolks1.mp3', isPlaying: false }, 
+			{ id: 21, soundName: 'ROUGH STUFF FOLKS 2', soundURL: './sounds/roughstufffolks2.mp3', isPlaying: false },
+			{ id: 23, soundName: 'OHHH!', soundURL: './sounds/boomoh.mp3', isPlaying: false },
+			{ id: 25, soundName: 'I\'M A VIBE GUY', soundURL: './sounds/imavibeguy.mp3', isPlaying: false },
+	  	]};
 
 
   }
