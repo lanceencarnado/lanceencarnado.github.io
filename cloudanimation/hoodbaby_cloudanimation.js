6d3aaa2b8b9b156081f6222f54d952cc08918f01
lscorrcho <l.scorrcho@gmail.com>
2019-09-06 00:12:35 -0400

    cloud animation test

---
+++ cloudanimation/hoodbaby_cloudanimation.js
@@ -0,0 +1,128 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [];
+
+
+// symbols:
+
+
+
+(lib.tes = function() {
+	this.initialize(img.tes);
+}).prototype = p = new cjs.Bitmap();
+p.nominalBounds = new cjs.Rectangle(0,0,4120,371);// helper functions:
+
+function mc_symbol_clone() {
+	var clone = this._cloneProps(new this.constructor(this.mode, this.startPosition, this.loop));
+	clone.gotoAndStop(this.currentFrame);
+	clone.paused = this.paused;
+	clone.framerate = this.framerate;
+	return clone;
+}
+
+function getMCSymbolPrototype(symbol, nominalBounds, frameBounds) {
+	var prototype = cjs.extend(symbol, cjs.MovieClip);
+	prototype.clone = mc_symbol_clone;
+	prototype.nominalBounds = nominalBounds;
+	prototype.frameBounds = frameBounds;
+	return prototype;
+	}
+
+
+(lib.Symbol1 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Layer_1
+	this.instance = new lib.tes();
+	this.instance.parent = this;
+	this.instance.setTransform(0,0,1.396,1.396);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
+
+}).prototype = getMCSymbolPrototype(lib.Symbol1, new cjs.Rectangle(0,0,5752.5,518), null);
+
+
+// stage content:
+(lib.hoodbaby_cloudanimation = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// cloud
+	this.instance = new lib.Symbol1();
+	this.instance.parent = this;
+	this.instance.setTransform(2876.2,259,1,1,0,0,0,2876.2,259);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1).to({x:2868.3},0).wait(1).to({x:2860.4},0).wait(1).to({x:2852.5},0).wait(1).to({x:2844.6},0).wait(1).to({x:2836.7},0).wait(1).to({x:2828.8},0).wait(1).to({x:2820.8},0).wait(1).to({x:2812.9},0).wait(1).to({x:2805},0).wait(1).to({x:2797.1},0).wait(1).to({x:2789.2},0).wait(1).to({x:2781.3},0).wait(1).to({x:2773.4},0).wait(1).to({x:2765.4},0).wait(1).to({x:2757.5},0).wait(1).to({x:2749.6},0).wait(1).to({x:2741.7},0).wait(1).to({x:2733.8},0).wait(1).to({x:2725.9},0).wait(1).to({x:2717.9},0).wait(1).to({x:2710},0).wait(1).to({x:2702.1},0).wait(1).to({x:2694.2},0).wait(1).to({x:2686.3},0).wait(1).to({x:2678.4},0).wait(1).to({x:2670.5},0).wait(1).to({x:2662.5},0).wait(1).to({x:2654.6},0).wait(1).to({x:2646.7},0).wait(1).to({x:2638.8},0).wait(1).to({x:2630.9},0).wait(1).to({x:2623},0).wait(1).to({x:2615},0).wait(1).to({x:2607.1},0).wait(1).to({x:2599.2},0).wait(1).to({x:2591.3},0).wait(1).to({x:2583.4},0).wait(1).to({x:2575.5},0).wait(1).to({x:2567.6},0).wait(1).to({x:2559.6},0).wait(1).to({x:2551.7},0).wait(1).to({x:2543.8},0).wait(1).to({x:2535.9},0).wait(1).to({x:2528},0).wait(1).to({x:2520.1},0).wait(1).to({x:2512.1},0).wait(1).to({x:2504.2},0).wait(1).to({x:2496.3},0).wait(1).to({x:2488.4},0).wait(1).to({x:2480.5},0).wait(1).to({x:2472.6},0).wait(1).to({x:2464.7},0).wait(1).to({x:2456.7},0).wait(1).to({x:2448.8},0).wait(1).to({x:2440.9},0).wait(1).to({x:2433},0).wait(1).to({x:2425.1},0).wait(1).to({x:2417.2},0).wait(1).to({x:2409.2},0).wait(1).to({x:2401.3},0).wait(1).to({x:2393.4},0).wait(1).to({x:2385.5},0).wait(1).to({x:2377.6},0).wait(1).to({x:2369.7},0).wait(1).to({x:2361.8},0).wait(1).to({x:2353.8},0).wait(1).to({x:2345.9},0).wait(1).to({x:2338},0).wait(1).to({x:2330.1},0).wait(1).to({x:2322.2},0).wait(1).to({x:2314.3},0).wait(1).to({x:2306.3},0).wait(1).to({x:2298.4},0).wait(1).to({x:2290.5},0).wait(1).to({x:2282.6},0).wait(1).to({x:2274.7},0).wait(1).to({x:2266.8},0).wait(1).to({x:2258.9},0).wait(1).to({x:2250.9},0).wait(1).to({x:2243},0).wait(1).to({x:2235.1},0).wait(1).to({x:2227.2},0).wait(1).to({x:2219.3},0).wait(1).to({x:2211.4},0).wait(1).to({x:2203.4},0).wait(1).to({x:2195.5},0).wait(1).to({x:2187.6},0).wait(1).to({x:2179.7},0).wait(1).to({x:2171.8},0).wait(1).to({x:2163.9},0).wait(1).to({x:2156},0).wait(1).to({x:2148},0).wait(1).to({x:2140.1},0).wait(1).to({x:2132.2},0).wait(1).to({x:2124.3},0).wait(1).to({x:2116.4},0).wait(1).to({x:2108.5},0).wait(1).to({x:2100.5},0).wait(1).to({x:2092.6},0).wait(1).to({x:2084.7},0).wait(1).to({x:2076.8},0).wait(1).to({x:2068.9},0).wait(1).to({x:2061},0).wait(1).to({x:2053.1},0).wait(1).to({x:2045.1},0).wait(1).to({x:2037.2},0).wait(1).to({x:2029.3},0).wait(1).to({x:2021.4},0).wait(1).to({x:2013.5},0).wait(1).to({x:2005.6},0).wait(1).to({x:1997.6},0).wait(1).to({x:1989.7},0).wait(1).to({x:1981.8},0).wait(1).to({x:1973.9},0).wait(1).to({x:1966},0).wait(1).to({x:1958.1},0).wait(1).to({x:1950.2},0).wait(1).to({x:1942.2},0).wait(1).to({x:1934.3},0).wait(1).to({x:1926.4},0).wait(1).to({x:1918.5},0).wait(1).to({x:1910.6},0).wait(1).to({x:1902.7},0).wait(1).to({x:1894.8},0).wait(1).to({x:1886.8},0).wait(1).to({x:1878.9},0).wait(1).to({x:1871},0).wait(1).to({x:1863.1},0).wait(1).to({x:1855.2},0).wait(1).to({x:1847.3},0).wait(1).to({x:1839.3},0).wait(1).to({x:1831.4},0).wait(1).to({x:1823.5},0).wait(1).to({x:1815.6},0).wait(1).to({x:1807.7},0).wait(1).to({x:1799.8},0).wait(1).to({x:1791.9},0).wait(1).to({x:1783.9},0).wait(1).to({x:1776},0).wait(1).to({x:1768.1},0).wait(1).to({x:1760.2},0).wait(1).to({x:1752.3},0).wait(1).to({x:1744.4},0).wait(1).to({x:1736.4},0).wait(1).to({x:1728.5},0).wait(1).to({x:1720.6},0).wait(1).to({x:1712.7},0).wait(1).to({x:1704.8},0).wait(1).to({x:1696.9},0).wait(1).to({x:1689},0).wait(1).to({x:1681},0).wait(1).to({x:1673.1},0).wait(1).to({x:1665.2},0).wait(1).to({x:1657.3},0).wait(1).to({x:1649.4},0).wait(1).to({x:1641.5},0).wait(1).to({x:1633.5},0).wait(1).to({x:1625.6},0).wait(1).to({x:1617.7},0).wait(1).to({x:1609.8},0).wait(1).to({x:1601.9},0).wait(1).to({x:1594},0).wait(1).to({x:1586.1},0).wait(1).to({x:1578.1},0).wait(1).to({x:1570.2},0).wait(1).to({x:1562.3},0).wait(1).to({x:1554.4},0).wait(1).to({x:1546.5},0).wait(1).to({x:1538.6},0).wait(1).to({x:1530.6},0).wait(1).to({x:1522.7},0).wait(1).to({x:1514.8},0).wait(1).to({x:1506.9},0).wait(1).to({x:1499},0).wait(1).to({x:1491.1},0).wait(1).to({x:1483.2},0).wait(1).to({x:1475.2},0).wait(1).to({x:1467.3},0).wait(1).to({x:1459.4},0).wait(1).to({x:1451.5},0).wait(1).to({x:1443.6},0).wait(1).to({x:1435.7},0).wait(1).to({x:1427.7},0).wait(1).to({x:1419.8},0).wait(1).to({x:1411.9},0).wait(1).to({x:1404},0).wait(1).to({x:1396.1},0).wait(1).to({x:1388.2},0).wait(1).to({x:1380.3},0).wait(1).to({x:1372.3},0).wait(1).to({x:1364.4},0).wait(1).to({x:1356.5},0).wait(1).to({x:1348.6},0).wait(1).to({x:1340.7},0).wait(1).to({x:1332.8},0).wait(1).to({x:1324.8},0).wait(1).to({x:1316.9},0).wait(1).to({x:1309},0).wait(1).to({x:1301.1},0).wait(1).to({x:1293.2},0).wait(1).to({x:1285.3},0).wait(1).to({x:1277.4},0).wait(1).to({x:1269.4},0).wait(1).to({x:1261.5},0).wait(1).to({x:1253.6},0).wait(1).to({x:1245.7},0).wait(1).to({x:1237.8},0).wait(1).to({x:1229.9},0).wait(1).to({x:1221.9},0).wait(1).to({x:1214},0).wait(1).to({x:1206.1},0).wait(1).to({x:1198.2},0).wait(1).to({x:1190.3},0).wait(1).to({x:1182.4},0).wait(1).to({x:1174.5},0).wait(1).to({x:1166.5},0).wait(1).to({x:1158.6},0).wait(1).to({x:1150.7},0).wait(1).to({x:1142.8},0).wait(1).to({x:1134.9},0).wait(1).to({x:1127},0).wait(1).to({x:1119},0).wait(1).to({x:1111.1},0).wait(1).to({x:1103.2},0).wait(1).to({x:1095.3},0).wait(1).to({x:1087.4},0).wait(1).to({x:1079.5},0).wait(1).to({x:1071.6},0).wait(1).to({x:1063.6},0).wait(1).to({x:1055.7},0).wait(1).to({x:1047.8},0).wait(1).to({x:1039.9},0).wait(1).to({x:1032},0).wait(1).to({x:1024.1},0).wait(1).to({x:1016.1},0).wait(1).to({x:1008.2},0).wait(1).to({x:1000.3},0).wait(1).to({x:992.4},0).wait(1).to({x:984.5},0).wait(1).to({x:976.6},0).wait(1).to({x:968.7},0).wait(1).to({x:960.7},0).wait(1).to({x:952.8},0).wait(1).to({x:944.9},0).wait(1).to({x:937},0).wait(1).to({x:929.1},0).wait(1).to({x:921.2},0).wait(1).to({x:913.3},0).wait(1).to({x:905.3},0).wait(1).to({x:897.4},0).wait(1).to({x:889.5},0).wait(1).to({x:881.6},0).wait(1).to({x:873.7},0).wait(1).to({x:865.8},0).wait(1).to({x:857.8},0).wait(1).to({x:849.9},0).wait(1).to({x:842},0).wait(1).to({x:834.1},0).wait(1).to({x:826.2},0).wait(1).to({x:818.3},0).wait(1).to({x:810.4},0).wait(1).to({x:802.4},0).wait(1).to({x:794.5},0).wait(1).to({x:786.6},0).wait(1).to({x:778.7},0).wait(1).to({x:770.8},0).wait(1).to({x:762.9},0).wait(1).to({x:754.9},0).wait(1).to({x:747},0).wait(1).to({x:739.1},0).wait(1).to({x:731.2},0).wait(1).to({x:723.3},0).wait(1).to({x:715.4},0).wait(1).to({x:707.5},0).wait(1).to({x:699.5},0).wait(1).to({x:691.6},0).wait(1).to({x:683.7},0).wait(1).to({x:675.8},0).wait(1).to({x:667.9},0).wait(1).to({x:660},0).wait(1).to({x:652},0).wait(1).to({x:644.1},0).wait(1).to({x:636.2},0).wait(1).to({x:628.3},0).wait(1).to({x:620.4},0).wait(1).to({x:612.5},0).wait(1).to({x:604.6},0).wait(1).to({x:596.6},0).wait(1).to({x:588.7},0).wait(1).to({x:580.8},0).wait(1).to({x:572.9},0).wait(1).to({x:565},0).wait(1).to({x:557.1},0).wait(1).to({x:549.1},0).wait(1).to({x:541.2},0).wait(1).to({x:533.3},0).wait(1).to({x:525.4},0).wait(1).to({x:517.5},0).wait(1).to({x:509.6},0).wait(1).to({x:501.7},0).wait(1).to({x:493.7},0).wait(1).to({x:485.8},0).wait(1).to({x:477.9},0).wait(1).to({x:470},0).wait(1).to({x:462.1},0).wait(1).to({x:454.2},0).wait(1).to({x:446.2},0).wait(1).to({x:438.3},0).wait(1).to({x:430.4},0).wait(1).to({x:422.5},0).wait(1).to({x:414.6},0).wait(1).to({x:406.7},0).wait(1).to({x:398.8},0).wait(1).to({x:390.8},0).wait(1).to({x:382.9},0).wait(1).to({x:375},0).wait(1).to({x:367.1},0).wait(1).to({x:359.2},0).wait(1).to({x:351.3},0).wait(1).to({x:343.3},0).wait(1).to({x:335.4},0).wait(1).to({x:327.5},0).wait(1).to({x:319.6},0).wait(1).to({x:311.7},0).wait(1).to({x:303.8},0).wait(1).to({x:295.9},0).wait(1).to({x:287.9},0).wait(1).to({x:280},0).wait(1).to({x:272.1},0).wait(1).to({x:264.2},0).wait(1).to({x:256.3},0).wait(1).to({x:248.4},0).wait(1).to({x:240.4},0).wait(1).to({x:232.5},0).wait(1).to({x:224.6},0).wait(1).to({x:216.7},0).wait(1).to({x:208.8},0).wait(1).to({x:200.9},0).wait(1).to({x:193},0).wait(1).to({x:185},0).wait(1).to({x:177.1},0).wait(1).to({x:169.2},0).wait(1).to({x:161.3},0).wait(1).to({x:153.4},0).wait(1).to({x:145.5},0).wait(1).to({x:137.5},0).wait(1).to({x:129.6},0).wait(1).to({x:121.7},0).wait(1).to({x:113.8},0).wait(1).to({x:105.9},0).wait(1).to({x:98},0).wait(1).to({x:90.1},0).wait(1).to({x:82.1},0).wait(1).to({x:74.2},0).wait(1).to({x:66.3},0).wait(1).to({x:58.4},0).wait(1).to({x:50.5},0).wait(1).to({x:42.6},0).wait(1).to({x:34.6},0).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(1330,259,5752.5,518);
+// library properties:
+lib.properties = {
+	id: '7F2593A2D0CE405DB1EB935B5D0312AF',
+	width: 2660,
+	height: 518,
+	fps: 24,
+	color: "#FFFFFF",
+	opacity: 0.00,
+	manifest: [
+		{src:"images/tes.png?1567742988914", id:"tes"}
+	],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['7F2593A2D0CE405DB1EB935B5D0312AF'] = {
+	getStage: function() { return exportRoot.getStage(); },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
