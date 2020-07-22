e5ad96d58d87776b1c5f58c36239f5cd5df94fbc
lscorrcho <lance.encarnado@gmail.com>
2020-07-22 00:44:43 -0400

    gods kid done curtain for all 4 About Us pages (day/night/day mobile/night mobile)

---
+++ gk/curtain/assets/curtain_night_mobile/curtain_night_mobile.js
@@ -0,0 +1,156 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [
+		{name:"curtain_night_mobile_atlas_", frames: [[0,820,1600,818],[0,1640,1600,818],[0,0,1600,818]]}
+];
+
+
+// symbols:
+
+
+
+(lib.Transition_B1_night = function() {
+	this.spriteSheet = ss["curtain_night_mobile_atlas_"];
+	this.gotoAndStop(0);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Transition_B2_night = function() {
+	this.spriteSheet = ss["curtain_night_mobile_atlas_"];
+	this.gotoAndStop(1);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.Transition_B3_night = function() {
+	this.spriteSheet = ss["curtain_night_mobile_atlas_"];
+	this.gotoAndStop(2);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.cherubs = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Layer_1
+	this.instance = new lib.Transition_B1_night();
+	this.instance.parent = this;
+	this.instance.setTransform(-805,-384);
+
+	this.instance_1 = new lib.Transition_B2_night();
+	this.instance_1.parent = this;
+	this.instance_1.setTransform(-805,-384);
+
+	this.instance_2 = new lib.Transition_B3_night();
+	this.instance_2.parent = this;
+	this.instance_2.setTransform(-805,-384);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance}]}).to({state:[{t:this.instance_1}]},3).to({state:[{t:this.instance_2}]},3).wait(3));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-805,-384,1600,818);
+
+
+(lib.Tween1 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Layer_1
+	this.instance = new lib.cherubs();
+	this.instance.parent = this;
+	this.instance.setTransform(0,360.8,0.237,0.237,0,0,0,-5,25.1);
+
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#000000").s().p("EgdrA7iMAAAh3DMA7XAAAMAAAB3Dg");
+	this.shape.setTransform(0,-76.9);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.shape},{t:this.instance}]}).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-190,-457.9,380,915.8);
+
+
+// stage content:
+(lib.curtain_night_mobile = function(mode,startPosition,loop) {
+if (loop == null) { loop = false; }	this.initialize(mode,startPosition,loop,{});
+
+	// Layer_1
+	this.instance = new lib.Tween1("synched",0);
+	this.instance.parent = this;
+	this.instance.setTransform(190,457.9);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).to({y:-440.4},59).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(190,450,380,915.8);
+// library properties:
+lib.properties = {
+	id: 'E276715C7F2E4603AB89C66C3520008E',
+	width: 380,
+	height: 900,
+	fps: 30,
+	color: "#FFFFFF",
+	opacity: 0.00,
+	manifest: [
+		{src:"images/curtain_night_mobile_atlas_.png", id:"curtain_night_mobile_atlas_"}
+	],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['E276715C7F2E4603AB89C66C3520008E'] = {
+	getStage: function() { return exportRoot.getStage(); },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
