ee4eba0ebaf00143e1bf01458a7f909d3db74883
lscorrcho <lance.encarnado@gmail.com>
2021-09-30 14:50:50 -0400

    all 9/28 notes addressed - removed a lot of hover animations, change Try Again button to not refresh the page

--- LILDURK/js/main.js
+++ LILDURK/js/main.js
@@ -1,23 +1,5 @@
 const nicknames = ["The Blessing","The Wave","The Threat","The Giver","The Clown","The Answer","The Speaker","The Menace","The Hater","The Vision","The Hood Saver","The Vibe","The Grandson","The Goat","The Voice","The Gap","The Hope","The Bull","The Sound","The Trenches","The Hero","The Player","The Family","The Ride","The Persuader","The Ear","The Problem","The Twin","The Shooter","The Fighter","The Process","The Genius","The Cook","The Decade","The Hope","The Snake","The Bucket","The Crown","The Jumper","The Freak","The King","The Rat","The Goblin","The Sickness","The Cure","The Teacher","The Student","The Bag","The Ditch","The Crybaby","The Heartless","The Incompetent","The Villain","The Mask ","The Preacher ","The Babysitter","The Day One","The Boss","The President","The Baby ","The Clueless","The Captain","The Joker","The Chief","The CEO","The Mastermind"]
 
-function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                return allText;
-            }
-        }
-    }
-    rawFile.send(null);
-}
-
 function generateNickname() {
 	// check that all fields are filled
 	for (const el of document.getElementById('info_entry').querySelectorAll("[required]")) {
@@ -40,8 +22,11 @@ function generateNickname() {
 	$('.result').fadeIn("slow");
 }
 
-// DON'T USE UNLESS READING FROM TEXT FILE
-function readNicknames() {
-	var text = readTextFile("js/nicknames.txt");
-	console.log(text);
+function resetPage() {
+	$('#fname').val('');
+	$('#lname').val('');
+	$('#zodiac').prop('selectedIndex',0);
+
+	$('.result').hide();
+	$('.info_entry').fadeIn("slow");
 }
