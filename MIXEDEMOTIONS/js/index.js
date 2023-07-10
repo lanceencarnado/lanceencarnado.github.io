a4813430036040141984b9baef41aff36e5ec55b
lscorrcho <lance.encarnado@gmail.com>
2023-07-10 08:07:39 -0700

    fixed a bunch of crap

--- MIXEDEMOTIONS/js/index.js
+++ MIXEDEMOTIONS/js/index.js
@@ -1,11 +1,12 @@
 function fadeOutContainer(containerID) {
-	$(containerID).css({
-	   'filter'         : 'blur(4px)',
-	   '-webkit-filter' : 'blur(4px)',
-	   '-moz-filter'    : 'blur(4px)',
-	   '-o-filter'      : 'blur(4px)',
-	   '-ms-filter'     : 'blur(4px)'
-	});
+	// NO BLUR FOR NOW
+	// $(containerID).css({
+	//    'filter'         : 'blur(4px)',
+	//    '-webkit-filter' : 'blur(4px)',
+	//    '-moz-filter'    : 'blur(4px)',
+	//    '-o-filter'      : 'blur(4px)',
+	//    '-ms-filter'     : 'blur(4px)'
+	// });
 	$(containerID).css({'opacity': '0'});
 	setTimeout(() => {
 	  $(containerID).css({'display': 'none'});
@@ -16,13 +17,14 @@ function fadeInContainer(containerID) {
 	setTimeout(() => {
 		$(containerID).css({'display': 'flex'});
 		setTimeout(() => {
-			$(containerID).css({
-			   'filter'         : 'blur(0px)',
-			   '-webkit-filter' : 'blur(0px)',
-			   '-moz-filter'    : 'blur(0px)',
-			   '-o-filter'      : 'blur(0px)',
-			   '-ms-filter'     : 'blur(0px)'
-			});
+			// NO BLUR FOR NOW
+			// $(containerID).css({
+			//    'filter'         : 'blur(0px)',
+			//    '-webkit-filter' : 'blur(0px)',
+			//    '-moz-filter'    : 'blur(0px)',
+			//    '-o-filter'      : 'blur(0px)',
+			//    '-ms-filter'     : 'blur(0px)'
+			// });
 			$(containerID).css({'opacity': '1'});
 		}, "50");
 	}, "1301");
@@ -120,6 +122,7 @@ setTimeout(() => {
 }, 1000);
 
 setTimeout(() => {
+	$("#startButton").css({'pointer-events': 'auto'});
 	$("#startButton").css({'opacity': '1'});
 }, 6200);
 
