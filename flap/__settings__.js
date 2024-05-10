9298cc02c4f5c62bf3d43ace4f154016dfb50a52
lanceencarnado <lance.encarnado@gmail.com>
2024-05-10 16:09:36 -0400

    re-add fb game

---
+++ flap/__settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1997379.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 177868044, 177868079, 177868043, 177868045, 177868042, 177868051, 177868047, 177868081, 177868049, 177868053, 177868055, 177868054, 177868074, 177868019, 177868080 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
