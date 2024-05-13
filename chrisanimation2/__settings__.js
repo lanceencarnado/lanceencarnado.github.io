055759eb8d40069cc5fc7c583e985760656e5201
lanceencarnado <lance.encarnado@gmail.com>
2024-05-13 18:28:16 -0400

    chris animation 2

---
+++ chrisanimation2/__settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1998949.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 178279201, 178279286, 178279190, 178279187, 178279283, 178279278, 178279288, 178279284, 178279261, 178279269, 178279183, 178279280, 178279260, 178279277, 178279197, 178279194 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
